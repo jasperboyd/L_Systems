57e50616207b77f533c71a49d49439c7b30eea6c
Jasper Boyd <jasperboyd@lavabit.com>
2013-03-22 00:40:49 -0400

    implemented process_line()

--- parser.cpp
+++ parser.cpp
@@ -6,13 +6,20 @@
 #include <vector> 
 using namespace std; 
 
-char HSH = '#';   
+bool start; 
+
+char HSH = '#'; 
+char SPACE = ' ';
 string LEN = "len:";
 int SEG_LEN; 
 string ITER = "iter:";
+int ITERATIONS;
 string ROT = "rot:";
+vector<float> ROTATIONS;
 string REP = "rep:"; 
 string START = "start:";
+//"initial turtle string" 
+string INIT_STR; 
 // +(theta) turn left by angle theta 
 char PLS = '+'; 
 // -(theta) turn right by angle theta
@@ -28,69 +35,73 @@ char BSL = '\\';
 // | is equivalent to +(180) -(180) 
 char VER = '|'; 
 
-//couldn't get atoi or stoi to work for me
-int ctoi (char c) { 
-    int i; 
-    if (c == '1'){
-        i = 1; 
-    } else if (c == '2') { 
-        i = 2; 
-    } else if (c == '3') { 
-        i = 3;
-    } else if (c == '4') {
-        i = 4; 
-    } else if (c == '5') { 
-        i = 5; 
-    } else if (c == '6') {
-        i = 6; 
-    } else if (c == '7') { 
-        i = 7; 
-    } else if (c == '8') { 
-        i = 8; 
-    } else if (c == '9') { 
-        i = 9; 
-    } else {
-        i = -1; //not an int
-    }
-    return i; 
-}
-
 void process_line ( string l ) {
+    if (start) { 
+        execute(l); 
+    } 
+    
     string t; //temp
     int d_pos; 
     int d_len; 
-    int l_length = l.length(); 
+    int l_length = l.length( ); 
     cout << "l_length = " << l_length << endl; 
     string l_sub4 = l.substr(0, 4);
     cout << "l_sub4 = " << l_sub4 << endl; 
     if (l[0] == HSH) {
         cout << "comment" << endl; 
         return; //ignore comment
-    } else if (l_sub4.compare (LEN) == 0) {
+    } else if (l_sub4.compare(LEN) == 0) {
         cout << "length" << endl;
         d_pos = 5;
         d_len = l_length - d_pos;
-        cout << "d_len = " << d_len << endl;
-        if (d_len == 1){
-            SEG_LEN = ctoi(l[d_pos]);
-        }
-        cout << "SEG_LEN = " << SEG_LEN << endl;
+        t = l.substr(d_pos, d_len); 
+        sscanf(t.c_str( ), "%d", &SEG_LEN);
+        cout << "SEG_LEN = " <<  SEG_LEN << endl;
         return; 
     } else if (l.substr(0, 5).compare (ITER) == 0) {
         cout << "iter" << endl; 
+        d_pos = 6; 
+        d_len = l_length - d_pos; 
+        t = l.substr(d_pos, d_len); 
+        sscanf(t.c_str( ), "%d", &ITERATIONS); 
+        cout << "ITERATIONS = " << ITERATIONS << endl; 
         return; 
     } else if (l_sub4.compare (ROT) == 0) { 
-        cout << "rot" << endl; 
+        d_pos = 5;
+        d_len = l_length - d_pos; 
+        t = l.substr(d_pos, d_len);
+        int count = 0; 
+        double  current; 
+        string c = "";
+        for (int i = 0; i < t.length(); i++){ 
+            if (t[i] == SPACE) {
+                current = ::atof(c.c_str( )); 
+                cout << "ROT current = " << current << endl; 
+                ROTATIONS.push_back(current);
+                count = 0;
+                c = ""; 
+            } else {
+                c += t[count++];
+            }//if/else
+        }//for
+        //remove below and turn into while loop for opti 
+        current = ::atof(c.c_str( )); 
+        cout << "ROT current = " << current << endl; 
+        ROTATIONS.push_back(current); 
         return;
     } else if (l_sub4.compare (REP) == 0) { 
         cout << "rep" << endl; 
         return; 
     } else if (l.substr(0, 6).compare (START) == 0 ) {
-        cout << "start" << endl; 
+        d_pos = 7;
+        d_len = l_length - d_pos; 
+        INIT_STR = l.substr(d_pos, d_len); 
+        cout << "INIT_STR = " << INIT_STR << endl; 
+        start = true; 
         return; 
     } else { 
         //cout << l[i] << endl;
-    }
+    }//if/else
 }//process_line
 
 int main ( ) {
@@ -101,6 +112,7 @@ int main ( ) {
     ifstream in (filename.c_str()); 
     if (in.is_open ( ) ) {
         cout << "File is open" << endl;
+        start = false; 
         while(in.good ( ) ) {
             getline (in, line); 
             process_line (line); 
